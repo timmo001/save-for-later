7b848cae798ff214f3b8687d6827798b050f1b04
Aidan Timson <aidan@timmo.dev>
2024-09-28 14:23:03 +0100

    Setup

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,4 +1,5 @@
-import { Save } from "lucide-react";
+import { storage } from "wxt/storage";
+import { Link, Trash } from "lucide-react";
 import { z } from "zod";
 
 import "./style.css";
@@ -8,31 +9,76 @@ import { Button } from "~/entrypoints/popup/components/ui/button";
 import { Separator } from "~/entrypoints/popup/components/ui/separator";
 
 const SavedItemSchema = z.object({
-  id: z.string(),
   position: z.number(),
   url: z.string(),
 });
 
 type SavedItem = z.infer<typeof SavedItemSchema>;
 
-const savedItems: Array<SavedItem> = [
-  {
-    id: "1",
-    position: 0,
-    url: "https://timmo.dev",
-  },
-  {
-    id: "2",
-    position: 1,
-    url: "https://vitejs.dev",
-  },
-];
-
 function App() {
+  const [savedItems, setSavedItems] = useState<Array<SavedItem>>();
+
+  useEffect(() => {
+    const unwatch = storage.watch<Array<SavedItem>>(
+      "sync:savedItems",
+      (newItems, oldItems) => {
+        console.log("Items changed:", { newItems, oldItems });
+      },
+    );
+
+    // if (!savedItems)
+    //   getSavedItems().then((items) => {
+    //     if (items) {
+    //       setSavedItems(items);
+    //     } else {
+    //       setSavedItems([]);
+    //     }
+    //   });
+
+    return () => {
+      unwatch();
+    };
+  }, []);
+
+  async function getSavedItems() {
+    return await storage.getItem<Array<SavedItem>>("sync:savedItems");
+  }
+
+  async function saveItem(item: SavedItem) {
+    if (!savedItems) {
+      await storage.setItem("sync:savedItems", [item]);
+      setSavedItems([item]);
+      return;
+    }
+
+    const newSavedItems = [...savedItems, item];
+    setSavedItems(newSavedItems);
+    await storage.setItem("sync:savedItems", newSavedItems);
+  }
+
   return (
     <ThemeProvider defaultTheme="system">
-      <main className="w-[320px] bg-gradient-to-b from-slate-400 to-white p-6 dark:from-slate-950 dark:to-black">
-        <h1 className="w-full text-2xl font-semibold tracking-tight text-center text-nowrap text-primary">
+      <main className="flex w-[320px] max-w-[420px] flex-col items-center justify-center bg-gradient-to-b from-slate-400 to-white p-6 dark:from-slate-950 dark:to-black">
+        <svg
+          className="w-1/3"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          version="1.1"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"
+            stroke="#6366f1"
+          />
+          <path
+            d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"
+            stroke="#8b5cf6"
+          />
+        </svg>
+        <h1 className="w-full mt-2 text-2xl font-semibold tracking-tight text-center text-nowrap text-primary">
           Save for Later
         </h1>
         <p className="w-full text-sm text-center">
@@ -40,8 +86,34 @@ function App() {
         </p>
 
         <section className="flex flex-row justify-center mt-6">
-          <Button variant="default">
-            <Save size={16} className="mr-2" />
+          <Button
+            variant="default"
+            onClick={() => {
+              // Get the browser tab URL and save it
+              browser.tabs
+                .query({
+                  active: true,
+                  currentWindow: true,
+                })
+                .then(([currentTab]) => {
+                  if (!currentTab) {
+                    console.warn("Cannot find currentTab:", currentTab);
+                    return;
+                  }
+
+                  if (!currentTab.url) {
+                    console.warn("Cannot find currentTab url:", currentTab);
+                    return;
+                  }
+
+                  saveItem({
+                    position: 0,
+                    url: currentTab.url,
+                  });
+                });
+            }}
+          >
+            <Link size={16} className="mr-2" />
             Save for later
           </Button>
         </section>
@@ -49,30 +121,30 @@ function App() {
 
         <section className="flex flex-col items-center">
           <h2 className="text-lg font-semibold text-primary">Saved Items</h2>
-          {savedItems.length < 1 ? (
-            <p className="text-sm text-center">You have no saved items.</p>
-          ) : (
+          {savedItems && savedItems.length > 0 ? (
             <ul className="w-full mt-6">
               {savedItems.map((item) => (
                 <li
-                  key={item.id}
+                  key={item.url}
                   className="flex flex-row items-center justify-between p-2 rounded-md bg-slate-300 dark:bg-slate-950"
                 >
-                  <p className="text-sm">
-                    <a
-                      href={item.url}
-                      referrerPolicy="no-referrer"
-                      target="_blank"
-                    >
-                      {item.url}
-                    </a>
-                  </p>
-                  <Button variant="default" size="sm">
-                    Remove
+                  <a
+                    className="flex-1 text-sm"
+                    href={item.url}
+                    referrerPolicy="no-referrer"
+                    target="_blank"
+                  >
+                    {item.url}
+                  </a>
+                  <Button variant="outline" size="sm">
+                    <Trash size={12} />
+                    <span className="sr-only">Remove</span>
                   </Button>
                 </li>
               ))}
             </ul>
+          ) : (
+            <p className="text-sm text-center">You have no saved items.</p>
           )}
         </section>
       </main>
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,8 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  manifest: {
+    name: "Save for Later",
+    permissions: ["storage", "tabs"],
+  },
 });
